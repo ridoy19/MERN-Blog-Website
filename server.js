ee81cb6ebf9dcd0265e363babfa915b0d0ad165f
Jakaria Hossain Ridoy <jh_ridoy@yahoo.com>
2020-08-12 20:42:00 +0600

    under construction :wrench:

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../client/build')));
+  app.use('/', express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
